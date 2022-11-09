08a84dc389560c27a579b680385a2cf83394935b
tonmoy <tonmoy3673@gmail.com>
2022-11-09 17:06:06 +0600

    set firebase env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAOj_y_WratiH-lyU4PgeNPi87v6ujRzMI",
-  authDomain: "health-coach-4ecad.firebaseapp.com",
-  projectId: "health-coach-4ecad",
-  storageBucket: "health-coach-4ecad.appspot.com",
-  messagingSenderId: "567339835085",
-  appId: "1:567339835085:web:67caa9af1105d5c9109384"
+    apiKey: process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
