7e3a5a1fc2384765838f4023aed50ec056c097d7
tonmoy <tonmoy3673@gmail.com>
2023-06-07 14:00:23 +0600

    added company

---
+++ src/Shared/Company/Company.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Company = () => {
+    return (
+        <div className='py-2'>
+            <div className="container py-5 d-none d-lg-block">
+              <h1 className="logo text-center">Trusted by over 800+ companies</h1>
+              <div className="row d-flex align-items-center justify-content-center py-4">
+                <div className="col-lg-2">
+                  <img src="https://i.ibb.co/mSdxjd8/company-1.png" className="img-fluid w-75" alt='company'/>
+                </div>
+                <div className="col-lg-2">
+                  <img src="https://i.ibb.co/4pTr5SW/company-2.png" className="img-fluid w-75" alt='company'/>
+                </div>
+                <div className="col-lg-2">
+                  <img src="https://i.ibb.co/hgFTPVn/company-3.png" className="img-fluid w-50" alt='company'/>
+                </div>
+                <div className="col-lg-2">
+                  <img src="https://i.ibb.co/VC5ccCN/company-5.png" className="img-fluid w-75" alt='company'/>
+                </div>
+                <div className="col-lg-2">
+                  <img src="https://i.ibb.co/nw0n5MG/company-4.png" className="img-fluid w-75" alt='company'/>
+                </div>
+                <div className="col-lg-2">
+                  <img src="https://i.ibb.co/NrG8SHg/company-6.png" className="img-fluid w-75" alt='company'/>
+                </div>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default Company;
